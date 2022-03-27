52d046e08f9f2014710665a3d219dc8bba540008
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-28 03:02:01 +0700

    program menampilkan kelipatan 2 Sampai 20 kecuali kelipatan 6

---
+++ src/Latihan06_MenampilkanKelipatan2Sampai20KecualiKelipatan6.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Latihan06_MenampilkanKelipatan2Sampai20KecualiKelipatan6 {
+    public static void main(String[] args) {
+        int i = 0;
+        int x = 0;
+        System.out.print("");
+        while(x<20){
+            if (i%3==0)
+                i++;
+            x = 2 * i;
+            System.out.print(" "+ x +", ");
+            i++;}
+        System.out.println(" ");
+    }
+}
